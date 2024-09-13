70751a9e6ac600d12c9918f064729afb974337fc
dudezi <a01036952858@gmail.com>
2024-09-13 09:27:00 +0900

    feat:도매인article/list생성

--- src/main/java/com/example/sbs_test1st_2/article/ArticleController.java
+++ src/main/java/com/example/sbs_test1st_2/article/ArticleController.java
@@ -1,8 +1,16 @@
 package com.example.sbs_test1st_2.article;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@RequestMapping("/article")
 @Controller
 public class ArticleController {
-
+    @GetMapping("/list")
+    @ResponseBody
+    public String articleList() {
+        return "article_list";
+    }
 }
--- src/main/java/com/example/sbs_test1st_2/article/ArticleRepository.java
+++ src/main/java/com/example/sbs_test1st_2/article/ArticleRepository.java
@@ -1,7 +1,8 @@
 package com.example.sbs_test1st_2.article;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class ArticleRepository {
+public interface ArticleRepository extends JpaRepository<Article, Integer> {
 }
