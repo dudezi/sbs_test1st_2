ac579cd69dc20fd88f9d216ef601b9023e6b9640
dudezi <a01036952858@gmail.com>
2024-09-13 09:10:03 +0900

    first commit

---
+++ src/main/java/com/example/sbs_test1st_2/SbsTest1st2Application.java
@@ -0,0 +1,13 @@
+package com.example.sbs_test1st_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbsTest1st2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbsTest1st2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sbs_test1st_2/SbsTest1st2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sbs_test1st_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbsTest1st2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
