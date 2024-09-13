89c68dec3dfffc33e596444de2f2d5b30764a709
dudezi <a01036952858@gmail.com>
2024-09-13 10:37:09 +0900

    feat:등록폼추가

--- src/main/java/com/example/sbs_test1st_2/article/ArticleController.java
+++ src/main/java/com/example/sbs_test1st_2/article/ArticleController.java
@@ -1,15 +1,35 @@
 package com.example.sbs_test1st_2.article;
 
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequestMapping("/article")
+@RequiredArgsConstructor
 @Controller
 public class ArticleController {
+    private final ArticleService articleService;
 
     @GetMapping("/list")
-    public String articleList() {
+    public String articleList(Model model) {
+        List<Article> articleList = this.articleService.getList();
+        model.addAttribute("articleList", articleList);
         return "article_list";
+    }
+
+    @GetMapping("/create")
+    public String articleCreate(ArticleForm articleForm) {
+        return "article_form";
+    }
+
+    @PostMapping("/create")
+    public String articleCreate(Model model, @PathVariable("id") Integer id, @Valid ArticleForm articleForm, BindingResult bindingResult) {
+
+        return "article_form";
     }
 }
--- src/main/java/com/example/sbs_test1st_2/article/ArticleService.java
+++ src/main/java/com/example/sbs_test1st_2/article/ArticleService.java
@@ -1,8 +1,37 @@
 package com.example.sbs_test1st_2.article;
 
+import com.example.sbs_test1st_2.DataNotFoundException;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+@RequiredArgsConstructor
 @Service
 public class ArticleService {
+    private final ArticleRepository articleRepository;
 
+    public List<Article> getList() {
+        return articleRepository.findAll();
+    }
+
+    public Article getArticle(Integer id) {
+        Optional<Article> oq = this.articleRepository.findById(id);
+        if (oq.isPresent()) {
+            return oq.get();
+        } else {
+            throw new DataNotFoundException("question not found");
+        }
+    }
+
+    public void create(String title, String content) {
+        Article a = new Article();
+        a.setTitle(title);
+        a.setContent(content);
+        a.setCreatedDate(LocalDateTime.now());
+
+        articleRepository.save(a);
+    }
 }
